90d9d2c8c4c9b0ddbb718fe4e75ec9464f807293
Henry Quinn <quinncuatro@Henrys-MacBook-Pro.local>
2018-10-08 19:00:42 -0400

    Scrubbing out some garbage.

--- src/components/EliNav.vue
+++ src/components/EliNav.vue
@@ -12,12 +12,6 @@
           <router-link class="nav-link" to="/">Home</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="/Chambers">Chambers</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/CourtFamily">Court Family</router-link>
-        </li>
-        <li class="nav-item">
           <router-link class="nav-link" to="/Forms">Forms</router-link>
         </li>
         <li class="nav-item">
@@ -25,12 +19,6 @@
         </li>
         <li class="nav-item">
           <router-link class="nav-link" to="/IT">IT</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/Jury">Jury</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/NextGen">NextGen</router-link>
         </li>
         <li class="nav-item">
           <router-link class="nav-link" to="/Operations">Operations</router-link>
@@ -47,93 +35,109 @@
 </template>
 
 <style lang="stylus">
-nav
-  margin-top -10px
+nav {
+  margin-top: -10px;
+}
 
-.nav-link
-  font-size 17px
+.nav-link {
+  font-size: 17px;
+}
 
-.navbar
-  background-color #981b1e
-  border-color #cd2026
+.navbar {
+  background-color: #981b1e;
+  border-color: #cd2026;
+}
 
-.navbar
-  color #ffffff
+.navbar {
+  color: #ffffff;
+}
 
-.navbar .navbar-brand:hover,
-.navbar .navbar-brand:focus
-  color #ffffff
+.navbar .navbar-brand:hover, .navbar .navbar-brand:focus {
+  color: #ffffff;
+}
 
-.navbar .navbar-text
-  color #ffffff
+.navbar .navbar-text {
+  color: #ffffff;
+}
 
-.navbar .navbar-nav > li > a
-  color #ffffff
-  padding-top 15px
-  padding-bottom 15px
+.navbar .navbar-nav > li > a {
+  color: #ffffff;
+  padding-top: 15px;
+  padding-bottom: 15px;
+}
 
-.navbar .navbar-nav > li > a:hover,
-.navbar .navbar-nav > li > a:focus
-  color #000000
-  background-color #b8383b
+.navbar .navbar-nav > li > a:hover, .navbar .navbar-nav > li > a:focus {
+  color: #000000;
+  background-color: #b8383b;
+}
 
-.navbar .navbar-nav > .active > a,
-.navbar .navbar-nav > .active > a:hover,
-.navbar .navbar-nav > .active > a:focus
-  color #ffffff
-  background-color #cd2026
+.navbar .navbar-nav > .active > a, .navbar .navbar-nav > .active > a:hover, .navbar .navbar-nav > .active > a:focus {
+  color: #ffffff;
+  background-color: #cd2026;
+}
 
-.navbar .navbar-nav > .open > a,
-.navbar .navbar-nav > .open > a:hover,
-.navbar .navbar-nav > .open > a:focus
-  color #ffffff
-  background-color #cd2026
+.navbar .navbar-nav > .open > a, .navbar .navbar-nav > .open > a:hover, .navbar .navbar-nav > .open > a:focus {
+  color: #ffffff;
+  background-color: #cd2026;
+}
 
-.navbar .navbar-toggle
-  border-color #ffffff
+.navbar .navbar-toggle {
+  border-color: #ffffff;
+}
 
-.navbar .navbar-toggle:hover,
-.navbar .navbar-toggle:focus
-  background-color #cd2026
+.navbar .navbar-toggle:hover, .navbar .navbar-toggle:focus {
+  background-color: #cd2026;
+}
 
-.navbar .navbar-toggle .icon-bar
-  background-color #ffffff
+.navbar .navbar-toggle .icon-bar {
+  background-color: #ffffff;
+}
 
-.navbar .navbar-collapse,
-.navbar .navbar-form
-  border-color #ffffff
+.navbar .navbar-collapse, .navbar .navbar-form {
+  border-color: #ffffff;
+}
 
-.navbar .navbar-link
-  color #ffffff
+.navbar .navbar-link {
+  color: #ffffff;
+}
 
-.navbar .navbar-link:hover
-  color #ffffff
+.navbar .navbar-link:hover {
+  color: #ffffff;
+}
 
-.navLink:link, .navLink:visited
-  text-decoration none
+.navLink:link, .navLink:visited {
+  text-decoration: none;
+}
 
-.navLinkFalse
-  cursor default
+.navLinkFalse {
+  cursor: default;
+}
 
-.navbar .navbar-nav
-  float none
-  vertical-align top
+.navbar .navbar-nav {
+  float: none;
+  vertical-align: top;
+}
 
-.navbar .navbar-collapse .navbar-nav #navbarSupportedContent
-  text-align center
+.navbar .navbar-collapse .navbar-nav #navbarSupportedContent {
+  text-align: center;
+}
 
-@media (max-width: 767px)
-  .navbar .navbar-nav .open .dropdown-menu > li > a
-    color #ffffff
-  .navbar .navbar-nav .open .dropdown-menu > li > a:hover,
-  .navbar .navbar-nav .open .dropdown-menu > li > a:focus
-    color #ffffff
-  .navbar .navbar-nav .open .dropdown-menu > .active > a,
-  .navbar .navbar-nav .open .dropdown-menu > .active > a:hover,
-  .navbar .navbar-nav .open .dropdown-menu > .active > a:focus
-    color #ffffff
-    background-color #cd2026
+@media (max-width: 767px) {
+  .navbar .navbar-nav .open .dropdown-menu > li > a {
+    color: #ffffff;
+  }
 
-.navbar-toggle
-  margin-right 25px
+  .navbar .navbar-nav .open .dropdown-menu > li > a:hover, .navbar .navbar-nav .open .dropdown-menu > li > a:focus {
+    color: #ffffff;
+  }
+
+  .navbar .navbar-nav .open .dropdown-menu > .active > a, .navbar .navbar-nav .open .dropdown-menu > .active > a:hover, .navbar .navbar-nav .open .dropdown-menu > .active > a:focus {
+    color: #ffffff;
+    background-color: #cd2026;
+  }
+}
+
+.navbar-toggle {
+  margin-right: 25px;
+}
 </style>
--- src/router.js
+++ src/router.js
@@ -6,64 +6,47 @@ Vue.use(Router);
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/Chambers',
-      name: 'Chambers',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Chambers.vue'),
-    },
-    {
-      path: '/CourtFamily',
-      name: 'CourtFamily',
-      component: () => import(/* webpackChunkName: "about" */ './views/CourtFamily.vue'),
-    },
-    {
-      path: '/Forms',
-      name: 'Forms',
-      component: () => import(/* webpackChunkName: "about" */ './views/Forms.vue'),
-    },
-    {
-      path: '/HR',
-      name: 'HR',
-      component: () => import(/* webpackChunkName: "about" */ './views/HR.vue'),
-    },
-    {
-      path: '/IT',
-      name: 'IT',
-      component: () => import(/* webpackChunkName: "about" */ './views/IT.vue'),
-    },
-    {
-      path: '/Jury',
-      name: 'Jury',
-      component: () => import(/* webpackChunkName: "about" */ './views/Jury.vue'),
-    },
-    {
-      path: '/NextGen',
-      name: 'NextGen',
-      component: () => import(/* webpackChunkName: "about" */ './views/NextGen.vue'),
-    },
-    {
-      path: '/Operations',
-      name: 'Operations',
-      component: () => import(/* webpackChunkName: "about" */ './views/Operations.vue'),
-    },
-    {
-      path: '/WhatsNew',
-      name: 'WhatsNew',
-      component: () => import(/* webpackChunkName: "about" */ './views/WhatsNew.vue'),
-    },
-    {
-      path: '/',
-      name: 'Home',
-      component: () => import(/* webpackChunkName: "about" */ './views/Operations.vue'),
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
+  routes: [{
+    path: '/Forms',
+    name: 'Forms',
+    component: () =>
+        import(/* webpackChunkName: "about" */ './views/Forms.vue'),
+  },
+  {
+    path: '/HR',
+    name: 'HR',
+    component: () =>
+        import(/* webpackChunkName: "about" */ './views/HR.vue'),
+  },
+  {
+    path: '/IT',
+    name: 'IT',
+    component: () =>
+        import(/* webpackChunkName: "about" */ './views/IT.vue'),
+  },
+  {
+    path: '/Operations',
+    name: 'Operations',
+    component: () =>
+        import(/* webpackChunkName: "about" */ './views/Operations.vue'),
+  },
+  {
+    path: '/WhatsNew',
+    name: 'WhatsNew',
+    component: () =>
+        import(/* webpackChunkName: "about" */ './views/WhatsNew.vue'),
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: () =>
+        import(/* webpackChunkName: "about" */ './views/Operations.vue'),
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () =>
+        import(/* webpackChunkName: "about" */ './views/About.vue'),
+  },
   ],
 });
